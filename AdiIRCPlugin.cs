a6e821b0f00c41bd3f3dc01cbddb5fd17137ced3
SReject <sreject@comcast.net>
2017-08-05 07:42:40 -0400

    Moving template to own branch; unpacking zip

---
+++ AdiIRCPlugin.cs
@@ -0,0 +1,31 @@
+﻿namespace $safeprojectname$
+{
+    using AdiIRCAPIv2.Interfaces;
+
+    public class $safeprojectname$ : IPlugin
+    {
+        public string Description { get { return "Description"; } }
+
+        public string Author { get { return "Bob Jones"; } }
+
+        public string Name { get { return "$safeprojectname$"; } }
+
+        public string Version { get { return "1.0"; } }
+
+        public string Email { get { return "bob@jones.com"; } }
+
+        public IPluginHost Host { get; set; }
+
+        public ITools Tools { get; set; }
+
+        public void Initialize()
+        {
+            // Called when the plugin is loaded, subscribe to host events here.
+        }
+
+        public void Dispose()
+        {
+            // Called when the plugin is unloaded/closed, do clean up here
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# AdiIRC API v2 Plugin Template  
+This is a starting template for creating AdiIRC plugins that make use of the apiv2.
